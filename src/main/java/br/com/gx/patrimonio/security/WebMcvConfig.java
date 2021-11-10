8a7068110d8276c3e912f544d287cade37763665
Gustavo Fior <gustavo_fior@outlook.com>
2021-11-10 14:27:08 -0300

    Minor updates

--- src/main/java/br/com/gx/patrimonio/security/WebMcvConfig.java
+++ src/main/java/br/com/gx/patrimonio/security/WebMcvConfig.java
@@ -2,7 +2,9 @@ package br.com.gx.patrimonio.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.gx.patrimonio.interceptor.InterceptadorDeAcessos;
@@ -10,6 +12,7 @@ import br.com.gx.patrimonio.repository.AcessoRepository;
 import br.com.gx.patrimonio.repository.UserRepository;
 
 @Configuration
+@EnableWebMvc
 public class WebMcvConfig implements WebMvcConfigurer {
 
 	@Autowired
@@ -21,6 +24,16 @@ public class WebMcvConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new InterceptadorDeAcessos(acessoRepository, userRepository)).addPathPatterns("/**");
+	}
+	
+	// Para acessar recursos estaticos
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.
+		addResourceHandler("resources/**")
+		.addResourceLocations("/public", "classpath:/static/")
+		.setCachePeriod(31556926);
+		
 	}
 
 }
